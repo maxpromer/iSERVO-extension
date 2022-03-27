aecc15ee3862feef7ed4ab8a1d75ab7ad0230c68
Max <sonthaya.nt@hotmail.com>
2022-03-27 20:43:55 +0700

    Fixed

--- blocks/generators.js
+++ blocks/generators.js
@@ -2,7 +2,7 @@ Blockly.Python['iservo'] = function (block) {
   Blockly.Python.definitions_['import_iSERVO'] = 'import iSERVO';
 
   var dropdown_pin = block.getFieldValue('pin');
-	var value_angle = Blockly.JavaScript.valueToCode(block, 'angle', Blockly.JavaScript.ORDER_ATOMIC);
+	var value_angle = Blockly.Python.valueToCode(block, 'angle', Blockly.Python.ORDER_ATOMIC);
 
   var code = `iSERVO.setAngle(${dropdown_pin}, ${value_angle})\n`;
   return code;
@@ -12,8 +12,8 @@ Blockly.Python['iservo_calibrate'] = function (block) {
   Blockly.Python.definitions_['import_iSERVO'] = 'import iSERVO';
 
   var dropdown_pin = block.getFieldValue('pin');
-	var value_min = Blockly.JavaScript.valueToCode(block, 'min', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_max = Blockly.JavaScript.valueToCode(block, 'max', Blockly.JavaScript.ORDER_ATOMIC);
+	var value_min = Blockly.Python.valueToCode(block, 'min', Blockly.Python.ORDER_ATOMIC);
+  var value_max = Blockly.Python.valueToCode(block, 'max', Blockly.Python.ORDER_ATOMIC);
 
   var code = `iSERVO.calibrate(${dropdown_pin}, ${value_min}, ${value_max})\n`;
   return code;
